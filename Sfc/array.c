6692f9742e88f1547c358be3735c44455af51a09
anesbouguessa13-cell <anesbouguessa13@gmail.com>
2026-04-18 01:36:14 +0200

    لي بقاو كملوهم نتوم

--- Sfc/array.c
+++ Sfc/array.c
@@ -47,12 +47,120 @@ int   deleteAt(int arr[], int* size, int index){
     
 
 int   linearSearch(int arr[], int size, int value){
+  
+    for (int i = 0; i < size; i++) { 
+        if (arr[i] == value) {
+            return i; 
+        }
+        
+    }
     
+    return -1; 
 }
-int   binarySearch(int arr[], int size, int value){}
-void  bubbleSort(int arr[], int size){}
-void  selectionSort(int arr[], int size){}
-void  insertionSort(int arr[], int size){}
+
+
+int   binarySearch(int arr[], int size, int value){
+    
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high) {
+        
+        int mid = low + (high - low) / 2;
+
+        
+        if (arr[mid] == value) {
+            return mid;
+        }
+
+        if (arr[mid] < value) {
+            low = mid + 1;
+        }
+        
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+
+
+void  bubbleSort(int arr[], int size){
+    
+    int i, j, temp;
+    int swapped;
+
+    for (i = 0; i < size - 1; i++) {
+        swapped = 0; 
+
+    
+        for (j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                
+                swapped = 1; 
+            }
+        }
+
+        
+        if (swapped == 0) {
+            break;
+        }
+    }
+
+
+}
+void  selectionSort(int arr[], int size){
+   
+    int i, j, minIndex, temp;
+
+    
+    for (i = 0; i < size - 1; i++) {
+        
+        minIndex = i;
+
+        
+        for (j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        
+        if (minIndex != i) {
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+
+void  insertionSort(int arr[], int size){
+    
+    int i, key, j;
+    
+    
+    for (i = 1; i < size; i++) {
+        key = arr[i]; 
+        j = i - 1;
+
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        
+        
+        arr[j + 1] = key;
+    }
+}
+
+
 void  mergeSort(int arr[], int left, int right){}
 void  quickSort(int arr[], int low, int high){}
 int   findMax(int arr[], int size){}
